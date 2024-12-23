9327ae45d48be6c3f827fcf18c7e04d8f92d5b76
NaveenRathore5094 <rathorenaveen121@gmail.com>
2024-12-23 17:37:12 +0530

    Project Completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'bgGrey':'rgb(32 35 39)',
+        'bgblue':"rgb(15 78 120) ",
+      }
+    },
+  },
+  plugins: [],
+}
+
